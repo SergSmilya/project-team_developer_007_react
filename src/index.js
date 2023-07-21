dfdb244038ca0682168331dc8caa3b57eae46377
Olekasndr Gubskiy <infernokgg@gmail.com>
2023-07-21 18:38:43 +0300

    all fonts
    button all Poppins ??

--- src/index.js
+++ src/index.js
@@ -5,29 +5,33 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import { persistor, store } from 'redux/store';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import './index.css';
-
+const theme = extendTheme({
+  fonts: {
+    body: `'Poppins', sans-serif`,
+  },
+});
 // import fonts
-import './fonts/Helvetica/Helvetica.ttf';
-import './fonts/Inter/Inter-VariableFont_slnt,wght.ttf';
-import './fonts/Montserrat/Montserrat-VariableFont_wght.ttf';
-import './fonts/Poppins/Poppins-Black.ttf';
-import './fonts/Poppins/Poppins-Bold.ttf';
-import './fonts/Poppins/Poppins-Medium.ttf';
-import './fonts/Poppins/Poppins-Regular.ttf';
-import './fonts/Poppins/Poppins-SemiBold.ttf';
+// import './fonts/Helvetica/Helvetica.ttf';
+// import './fonts/Inter/Inter-VariableFont_slnt,wght.ttf';
+// import './fonts/Montserrat/Montserrat-VariableFont_wght.ttf';
+// import './fonts/Poppins/Poppins-Black.ttf';
+// import './fonts/Poppins/Poppins-Bold.ttf';
+// import './fonts/Poppins/Poppins-Medium.ttf';
+// import './fonts/Poppins/Poppins-Regular.ttf';
+// import './fonts/Poppins/Poppins-SemiBold.ttf';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     {/* <React.StrictMode> */}
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter basename="/project-team_developer_007_react">
-            <App />
-          </BrowserRouter>
-        </PersistGate>
-      </Provider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/project-team_developer_007_react">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
     {/* </React.StrictMode> */}
   </ChakraProvider>
 );
